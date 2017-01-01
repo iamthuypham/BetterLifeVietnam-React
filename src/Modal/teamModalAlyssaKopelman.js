0d543421e198a7c5a6366e0d94325d72fa780955
Thuy <tpham045@hotmail.com>
2017-01-01 02:06:21 +0000

    Finish our-team page

---
+++ src/Modal/teamModalAlyssaKopelman.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
+
+const compModal = React.createClass({
+  getInitialState() {
+    return { showModal: false };
+  },
+
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
+  render() {
+    return (
+      <div>
+        <h3 style={{ color:'#00ba5d', cursor:'pointer' }} onClick={this.open}>Alyssa Kopelman<h5 style={{textIndent: '0'}}>Marketing Specialist</h5></h3>
+        <Modal show={this.state.showModal} onHide={this.close}>
+            <Modal.Header closeButton>
+                <Modal.Title style={{color: '#00ba5d'}}>Alyssa Kopelman</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+        			<p>“Better Life Vietnam & Tours For Books exists at the crossroads of my interests of education, children and tourism. I couldn’t be more thrilled to get involved with the organization and help bring a brighter and better future for the children and youth of Vietnam.”</p>
+      				<p>Alyssa was born and grew up in New York, U.S.A. With a strong passion for helping others and an avid love to travel, Alyssa is inspired to give back through her professional marketing and design skills. She is a graduate of the University of Delaware, earning a Bachelor of Science in Marketing with a minor in Advertising and a Bachelor of Arts in Communications.
+      				</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.close}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+});
+
+module.exports = compModal
--- src/scenes/OurTeam.js
+++ src/scenes/OurTeam.js
@@ -1,8 +1,95 @@
-const React = require('react')
+import React from 'react'
+import { Grid, Row, Col, Thumbnail} from 'react-bootstrap'
+
+import ThinhNguyenThumb from '../images/Thinh-Nguyen.jpg'
+import GregThumb from '../images/Greg.jpg'
+import HoanVuThumb from '../images/Hoan-Vu.jpg'
+import PhuongNguyenThumb from '../images/Phuong-Nguyen.jpg'
+import RobertDoThumb from '../images/Robert-Do.jpg'
+import AlyssaThumb from '../images/Alyssa.jpg'
+import BethanyThumb from '../images/Bethany.jpg'
+import SimonWalterThumb from '../images/Walter.jpg'
+import ThachNguyenThumb from '../images/Thach-Nguyen.jpg'
+
+import TeamModalThinh from '../Modal/teamModalThinh'
+import TeamModalAlyssaKopelman from '../Modal/teamModalAlyssaKopelman'
+import TeamModalBethanyTietjen from '../Modal/teamModalBethanyTietjen'
+import TeamModalGregHitchcock from '../Modal/teamModalGregHitchcock'
+import TeamModalPhuongNguyen from '../Modal/teamModalPhuongNguyen'
+import TeamModalRobertDo from '../Modal/teamModalRobertDo'
+import TeamModalSimonWalter from '../Modal/teamModalSimonWalter'
+import TeamModalThachNguyen from '../Modal/teamModalThachNguyen'
+import TeamModalVuThaoHoan from '../Modal/teamModalVuThaoHoan'
 
 const OurTeam = () => (
-  <div className='app-container'>
-    <div>Hello</div>
+  <div className='container'>
+    <Grid>
+    <Row style={{padding: '0 5%'}}>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px', maxWidth:'250px'}}>
+      <Thumbnail src={ ThinhNguyenThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalThinh></TeamModalThinh>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ SimonWalterThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalSimonWalter></TeamModalSimonWalter>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ GregThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalGregHitchcock></TeamModalGregHitchcock>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ RobertDoThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalRobertDo></TeamModalRobertDo>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ ThachNguyenThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalThachNguyen></TeamModalThachNguyen>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ AlyssaThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalAlyssaKopelman></TeamModalAlyssaKopelman>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ HoanVuThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalVuThaoHoan></TeamModalVuThaoHoan>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ PhuongNguyenThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalPhuongNguyen></TeamModalPhuongNguyen>
+        </div>
+      </Thumbnail>
+    </Col>
+    <Col xs={12} sm={4} style={{ maxHeight: '420px',maxWidth:'250px'}}>
+      <Thumbnail src={ BethanyThumb } alt="242x200">
+        <div className='center-top-row'>
+          <TeamModalBethanyTietjen></TeamModalBethanyTietjen>
+        </div>
+      </Thumbnail>
+    </Col>
+    </Row>
+  </Grid>
   </div>
 )
 
