dad12c7277c2d995210e9021914cbd239461a335
Thuy <tpham045@hotmail.com>
2017-01-20 06:58:33 +0000

    Remove unnecessary component

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import ClientApp from './ClientApp'
 import './index.css';
-// import './semantic-ui/semantic.min.css';
 
 ReactDOM.render(
   <ClientApp />,
