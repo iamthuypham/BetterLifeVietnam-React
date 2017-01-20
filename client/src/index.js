746a0a9ee9f2e19ca772dd0325c56294a3090bca
Thuy <tpham045@hotmail.com>
2017-01-20 06:52:48 +0000

    Fix UI

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ClientApp from './ClientApp'
 import './index.css';
-import './semantic-ui/semantic.min.css';
+// import './semantic-ui/semantic.min.css';
 
 ReactDOM.render(
   <ClientApp />,
