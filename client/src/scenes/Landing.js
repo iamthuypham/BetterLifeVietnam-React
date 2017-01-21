0cd75059f42bc4e97c1f3aee143a8e146e254ea1
Thuy <tpham045@hotmail.com>
2017-01-21 19:03:18 +0000

    Remove carousel

--- client/src/scenes/Landing.js
+++ client/src/scenes/Landing.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import MainCarousel from '../Carousel/MainCarousel'
+import { Image, Button } from 'react-bootstrap'
 import Highlight from '../Highlight/Highlight'
+import FreeBookLibraryPic from '../images/FreeBookLibrary.jpg'
+import './Landing.css'
 
 const Landing = () => (
   <div className='container-fluid'>
-    <MainCarousel />
+    <div className='center-top-col cover-container'>
+      <div className='cover-title'>
+        <h1>Working to improve education conditions for underprivileged children and youth in Vietnam</h1>
+        <Button>Donate Now</Button>
+      </div>
+      <Image className='cover-pic' src={FreeBookLibraryPic} />
+    </div>
     <Highlight />
   </div>
 )
