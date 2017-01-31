de0623e06e60321b9ab9a45242d9bebe915664d4
Thuy <tpham045@hotmail.com>
2017-01-31 04:21:16 +0000

    Change sponsor section background, add tooltip

--- client/src/Highlight/Sponsor.js
+++ client/src/Highlight/Sponsor.js
@@ -1,23 +1,32 @@
 import React from 'react'
-import { Row, Col, Image } from 'react-bootstrap'
+import { Row, Col, Image, Tooltip, OverlayTrigger } from 'react-bootstrap'
 
 import './Sponsor.css'
 import VDGLogo from '../images/VDG_logo.png'
 import HomeLogo from '../images/Home_logo.jpg'
 import WaterforeveryoneLogo from '../images/waterforeveryone_logo.png'
 
-const Sponsor = () => (
-  <Row>
-    <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
-      <div><a href='http://www.vietdantravel.com/' target="_blank"><Image src={VDGLogo}></Image></a></div>
-    </Col>
-    <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
-      <div style={{textAlign: 'center'}}><a href='https://www.waterforeveryone.org/en' target="_blank"><Image src={WaterforeveryoneLogo}></Image></a></div>
-    </Col>
-    <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
-      <div><a href='http://m.homehanoirestaurant.com/' target="_blank"><Image src={HomeLogo}></Image></a></div>
-      <div className='sponsorNote'><p style={{fontSize: '10px', color:'#777', textAlign:'center'}}>Home Hanoi Restaurant will donate 10 percent of dinner service proceeds to benefit children of Vietnam through the Better Life Vietnam organization. Please inform the restaurant at check in.</p></div>
-    </Col>
-  </Row>
-)
+const tooltip = (
+  <Tooltip id="tooltip">Home Hanoi Restaurant will donate 10 percent of dinner service proceeds to benefit children of Vietnam through the Better Life Vietnam organization. Please inform the restaurant at check in.</Tooltip>
+);
+
+class Sponsor extends React.Component {
+  render () {
+    return (
+      <Row>
+        <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
+          <div><a href='http://www.vietdantravel.com/' target="_blank"><Image src={VDGLogo}></Image></a></div>
+        </Col>
+        <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
+          <div style={{textAlign: 'center'}}><a href='https://www.waterforeveryone.org/en' target="_blank"><Image src={WaterforeveryoneLogo}></Image></a></div>
+        </Col>
+        <Col xs={12} sm={4} className='sponsorImageCol center-top-col'>
+          <OverlayTrigger placement="top" overlay={tooltip}>
+            <div><a href='http://m.homehanoirestaurant.com/' target="_blank"><Image src={HomeLogo}></Image></a></div>
+          </OverlayTrigger>
+        </Col>
+      </Row>
+    )
+  }
+}
 export default Sponsor
