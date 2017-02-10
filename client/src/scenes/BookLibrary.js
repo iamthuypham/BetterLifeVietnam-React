f952561c59ce2ad71273fc1cf5a43fe935f7baff
Thuy <tpham045@hotmail.com>
2017-02-10 17:49:36 +0000

    Proofread

--- client/src/scenes/BookLibrary.js
+++ client/src/scenes/BookLibrary.js
@@ -14,10 +14,8 @@ const BookLibrary = () => (
     </div>
       <div className='container' style={{paddingBottom: '20px'}}>
         <h1 style={{color: 'black'}}> The Need for Classroom Libraries </h1>
-        <p><strong>Limited access to books has been a severe problem in Vietnam for decades</strong>. When the country gained independence in 1945, 94% of the Vietnamese population was illiterate. Since national unification in 1975, the literacy rate has dramatically increased to more than 95%, though this figure varies significantly between cities and rural areas.
-		Reading culture in Vietnam&#39;s large cities is drastically changing, but the country&#39;s rural communities are still in desperate need for books. <strong>The reading levels of rural students are so low that the pupils read only one or two books</strong>, aside from textbooks, every year, while urban students read 20-30 books.</p>
-		<p>To solve the shortage of books in rural areas, promote a reading culture, and enrich knowledge, Better Life Vietnam (BLV) has teamed up with the Center for Knowledge Assistance and Community Development (CKACD) to provide access to books for underprivileged children.
-		The CKACD, founded by Mr. Nguyen Quang Thach, has successfully employed its persistent strategy of crowdfunding to <strong>build more than 10,000 bookcases for nearly 500,000 children in rural areas</strong>. The two teams, along with its local communities, have set <strong>the goal to offer book access to 15 million rural youths in Vietnam in the coming years</strong>.</p>
+        <p><strong>Limited access to books has been a severe problem in Vietnam for decades</strong>. While reading culture in metropolitan areas is drastically changing, the country's rural communities are still in desperate need for books. The reading levels of rural students are so low that the pupils read only one or two books, aside from textbooks, every year, while urban students read 20-30 books.</p>
+		<p>To solve the shortage of books in rural areas, promote a reading culture, and enrich knowledge, Better Life Vietnam (BLV) has teamed up with the Center for Knowledge Assistance and Community Development (CKACD) to provide access to books for underprivileged children. The CKACD, founded by Mr. Nguyen Quang Thach, has successfully <strong>built more than 10,000 bookcases for nearly 500,000 children in rural areas</strong>. The two teams aim to <strong>offer book access to 15 million rural youths in Vietnam in the coming years</strong>.</p>
     </div>
     <div className='implementLibrary'>
       <div className='col'>
@@ -26,16 +24,14 @@ const BookLibrary = () => (
       <div className='col' style={{backgroundColor: '#029AB1'}}>
         <h1> Implementation of Classroom Libraries </h1>
         <p>
-          <strong>The Books Library for Brighter Future project</strong> aims to provide free books for underprivileged children in Vietnam through classroom libraries. Each classroom will be equipped with a bookcase full of picture books, short novels, and textbooks, which students can use at school or bring home.</p>
-        <p>Students who have read the most weekly/monthly will be recognize. Workshops and writing competitions will also be organized to enhance reading and comprehension skills. Based on reading activities records, the program will try to identify characteristics of readers at each school, e.g. what kind of books they read, how many books each child reads per year. This information, along with collaboration with schools, will be used to better evaluate the operation of the libraries, as well as the needs of students.</p>
+          <strong>The Books Library for Brighter Future project</strong> provides free books for underprivileged children in Vietnam through classroom libraries. Each classroom will be equipped with a bookcase full of picture books, short novels, and textbooks, which students can use at school or bring home.</p>
+        <p>Students who have read the most weekly/monthly will be recognized. Workshops and writing competitions will also be organized to enhance reading and comprehension skills. Based on reading activities records, the program will try to identify characteristics of readers at each school, e.g. what kind of books they read, how many books each child reads per year. This information will be used to better evaluate the operation of the libraries, as well as the needs of the students.</p>
       </div>
     </div>
     <div className='monitorLibrary'>
       <div className='col' style={{backgroundColor: '#40AB48'}}>
         <h1>Monitoring and Evaluation</h1>
-        <p><strong>The Books Library for Brighter Future project</strong> is evaluated each year. These evaluations help improve the libraries’ inventory to better serve readers, as well as to provide evidence for the effectiveness and necessity of classroom libraries and policy advocacy.</p>
-        <p>Quantitative methods (e.g. using records from reading activities to calculate reading rates) and qualitative methods (e.g. interviewing readers) are used to improve the effectiveness of the program.
-        </p>
+        <p>Each year, the Books Library for Brighter Future project is evaluated through quantitative methods (e.g. using records from reading activities to calculate reading rates) and qualitative methods (e.g. interviewing readers). These evaluations help improve the libraries’ inventory to better serve readers, as well as to provide evidence for the effectiveness and necessity of classroom libraries.</p>
       </div>
       <div className='col'>
       </div>
