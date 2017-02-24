2b843dfcdae839203802d7db9b40ef037ffa526a
Thuy <tpham045@hotmail.com>
2017-02-24 16:41:22 +0000

    Close transporter

--- server.js
+++ server.js
@@ -52,10 +52,11 @@ app.post('/api/comments', function (req, res) {
     var errorToComment = 'Oh no! Something went wrong. Please email us directly at betterlifevietnam@gmail.com'
     if (error) {
       comments.push(errorToComment)
+      transporter.close()
       return console.log('Transporter Error: '+ error)
     } 
-    transporter.close();
     res.send(comments)
+    transporter.close()
     console.log('Message sent: ' + info.response)
   })
 })
